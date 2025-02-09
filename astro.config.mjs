4a55fdda8d16f66894f4d515ad35975c31e1f29b
sagar puniyani <126869215+Sagar-Puniyani@users.noreply.github.com>
2025-02-10 00:09:30 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://the-pickleball-game.vercel.app/",
   output: "server",
-  integrations: [ mdx(), tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap()],
   env: {
     schema: {
       OPENAI_API_KEY: envField.string({
